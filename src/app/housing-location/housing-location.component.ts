68f2a0ac58da091c4675241cc5ab2a1fa76eb68e
Victor Levesque <levesque.vic@gmail.com>
2024-02-15 16:30:38 +1100

    second commit

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   imports: [],
   template: `
     <p>
-      housing-location works!
+      housing-location works! Yay
     </p>
   `,
   styleUrl: './housing-location.component.css'
